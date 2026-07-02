1f33c29b434c204008c8673cd954f75a33aca583
Mirko <mirkohtcz@gmail.com>
2026-07-02 11:18:57 +0200

    fix

--- src/it/unipv/posw/Main.java
+++ src/it/unipv/posw/Main.java
@@ -1,16 +1,16 @@
 package it.unipv.posw;
 
 import it.unipv.posw.controller.FuelController;
-import it.unipv.posw.model.persistence.dao.DistributoreDAO;
+import it.unipv.posw.model.gestori.Gestore;
 import it.unipv.posw.view.View;
 
 public class Main {
 	
 	public static void main(String[] args) {
-        DistributoreDAO dao = new DistributoreDAO();
+
         View view = new View();
         
-        FuelController controller = new FuelController(dao, view);
+        FuelController controller = new FuelController(Gestore.getInstance(), view);
         
         controller.avvia();
     }
